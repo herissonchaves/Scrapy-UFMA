44d97c45625365c0b257d14ea10f5a52481738ea
mschonfinkel <marcos.schonfinkel@gmail.com>
2017-12-05 22:19:54 -0300

    Crawler dos servidores

--- ufma_crawler/items.py
+++ ufma_crawler/items.py
@@ -11,4 +11,7 @@ import scrapy
 class UfmaCrawlerItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-    pass
+    name = scrapy.Field()
+    url_detail = scrapy.Field()
+    url_fin = scrapy.Field()
+    salary = scrapy.Field()
--- ufma_crawler/spiders/servidores.py
+++ ufma_crawler/spiders/servidores.py
@@ -1,23 +1,32 @@
 # -*- coding: utf-8 -*-
-import scrapy
+from scrapy import Spider, Request
+# from scrapy.loader import ItemLoader
+from ufma_crawler.items import UfmaCrawlerItem
 
 
-class ServidoresSpider(scrapy.Spider):
+class ServidoresSpider(Spider):
     name = 'servidores'
     allowed_domains = ['www.portaldatransparencia.gov.br']
     start_urls = ['http://www.portaldatransparencia.gov.br/servidores/OrgaoLotacao-ListaServidores.asp?CodOrg=26272&Pagina=1']
 
     def parse(self, response):
-        nomes = response.xpath("//td/a/text()").extract()
-        hrefs = response.xpath("//td/a/@href").extract()
+        table_rows = response.xpath("//td/a")
+
+        nomes = table_rows.xpath("./text()").extract()
+        hrefs = table_rows.xpath("./@href").extract()
 
         for (nome, href) in zip(nomes, hrefs):
-            yield {
-                "nome": nome.rstrip(),
-                "url": response.urljoin(href)
-            }
+            item = UfmaCrawlerItem(name=nome.rstrip(),
+                                   url_detail=response.urljoin(href))
 
-        # proxima
+            # item.add_value('nome', nome.rstrip())
+            # item.add_value('url_detail', response.urljoin(href))
+
+            yield Request(item['url_detail'],
+                          callback=self.parse_detail,
+                          meta={'item': item})
+
+        # proxima pagina
 
         URL = self.start_urls[0][:-1]
 
@@ -27,7 +36,39 @@ class ServidoresSpider(scrapy.Spider):
 
         if numero_pagina != ultima_pagina:
             next_page = URL + str(int(numero_pagina) + 1)
-            yield scrapy.Request(next_page, callback=self.parse)
+            yield Request(next_page, callback=self.parse)
+
+
+    def parse_detail(self, response):
+        item = response.meta['item']
+
+        resumo = response.xpath("//div/*[@id='resumo']")
+
+        url_fin = resumo.xpath("./a/@href").extract_first()
+        url_fin = "http://" + self.allowed_domains[0] + url_fin
+
+        item['url_fin'] = url_fin
+
+        yield Request(url_fin, callback=self.parse_info, meta={'item': item})
+
+    def parse_info(self, response):
+        # Apenas salário por enquanto
+        item = response.meta['item']
+
+        table_rows = response.xpath('//tbody/tr[@class="remuneracaolinhatotalliquida"]')
+        salary = table_rows.xpath('./td[@class="colunaValor"]/text()').extract_first()
+
+        # Replaces 9.999,99 -> 9999.99
+        if salary:
+            salary = salary.replace(".", "").replace(",", ".")
+            item['salary'] = float(salary)
+        else:
+            item['salary'] = None
+
+        return item
+
+
+
 
 
 
